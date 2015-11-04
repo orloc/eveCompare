8f702790abe0baf481796f03f4c5ec53725bec0c
Grant Tepper <grant.tepper@gmail.com>
2015-11-03 19:34:35 -0500

    implements index - adds basic providers bootstrap and app file for seperation

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Pulls various parts of the application
+ */
+
+require_once __DIR__.'/bootstrap.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = getenv('APP_ENV') === 'dev' ? true : false;
+
+require __DIR__.'/providers.php';
+
+return $app;
+
+
+
